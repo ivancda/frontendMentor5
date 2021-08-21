4c47c839160f8a26ce3a8a03de243d7fcc7e1bf2
ivancda <ivancda@gmail.com>
2021-08-20 21:52:21 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Unix timestamp (in seconds) to count down to
+  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(twoDaysFromNow)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+  
+
+  // Show version number
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
